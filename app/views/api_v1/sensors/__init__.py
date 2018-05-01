71cc3ebcf22a8535e6953eeed4c170417364ebdd
GuoXinghua <cnbluesky@qq.com>
2018-05-01 19:16:04 +0800

    2018.3.5

--- app/views/api_v1/sensors/__init__.py
+++ app/views/api_v1/sensors/__init__.py
@@ -295,14 +295,14 @@ class SensorTimeView(Resource):
         if g.role.name == 'homeuser':
             if g.user.id == home.admin_user_id:
                 db.session.commit()
-               # return '添加成功', 200
-                return {'start_time': str(sensortime.start_time), 'type': str(type(sensortime.start_time))}, 200
+                return '添加成功', 200
+                #return {'start_time': str(sensortime.start_time), 'type': str(type(sensortime.start_time))}, 200
             else:
                 return '权限不足', 201
         else:
             db.session.commit()
-            # return '添加成功', 200
-            return {'start_time': str(sensortime.start_time), 'type': str(type(sensortime.start_time))}, 200
+            return '添加成功', 200
+            #return {'start_time': str(sensortime.start_time), 'type': str(type(sensortime.start_time))}, 200
 
 
 @api.route('/<start_time>/<end_time>/<sensorid>/maxvalue')
--- app/views/api_v1/sensors/model.py
+++ app/views/api_v1/sensors/model.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask_restplus import Model, fields
 from . import api
 
@@ -7,14 +9,12 @@ sensor_model = api.model('SensorModel', {
     'sensor_type': fields.Integer,
     'sensor_place': fields.String,
     'home_id':fields.String,
-    'start_time':fields.DateTime,
-    'end_time':fields.DateTime,
     'max_value':fields.Float,
     'set_type':fields.String
 })
 sensortime_model=api.model('SensorTime',{
     'id':fields.String,
     'sensor_id':fields.String,
-    'start_time':fields.DateTime,
-    'end_time':fields.DateTime
+    'start_time':fields.String(attribute=lambda x: x.start_time.strftime('%H:%M')),
+    'end_time':fields.String(attribute=lambda x: x.end_time.strftime('%H:%M'))
 })
