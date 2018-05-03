88dc87b4e9ef5f59814eb8dd8a168de9cebb4653
GuoXinghua <cnbluesky@qq.com>
2018-05-03 13:26:24 +0800

    2018.3.5

--- app/views/api_v1/useralarms/parser.py
+++ app/views/api_v1/useralarms/parser.py
@@ -2,7 +2,7 @@ from flask_restplus.reqparse import RequestParser
 
 useralarmrecord_parser=RequestParser()
 useralarmrecord_parser.add_argument('type',type=int,help='报警信息类型',required=True,location='form')
-useralarmrecord_parser.add_argument('content',type=str,help='报警内容',required=True,location='form')
+useralarmrecord_parser.add_argument('content',type=str,help='报警内容',required=False,location='form')
 useralarmrecord_parser.add_argument('user_id',type=str,help='用户id',required=True,location='form')
 useralarmrecord_parser.add_argument('home_id',type=str,help='家庭id',required=False,location='form')
 useralarmrecord_parser.add_argument('ins_id',type=str,help='机构id',required=False,location='form')
